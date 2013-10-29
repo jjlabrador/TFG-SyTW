c2d2d4757e225a8a14d4ec7a376e6cd0b9b5a7e8
Juan Jose <jjlabradorglez@gmail.com>
2013-10-29 17:37:35 +0000

    Añadiendo protección, redirecciones absolutas y codificación utf-8

--- app/tfg.rb
+++ app/tfg.rb
@@ -1,13 +1,14 @@
+#encoding: utf-8
 require 'sinatra/base'
 
 class MyApp < Sinatra::Base
   
   configure do
-    enable :logging           # o set :logging, true
-    set :dump_errors, true
-    set :raise_errors, false  #?
-    set :show_exceptions, true  
+    enable :logging, :dump_errors, :show_exceptions
+    set :raise_errors, false
     set :root, File.dirname(__FILE__)
+    enable :protection
+    enable :absolute_redirects                        # para cumplir con la RFC 2616 (HTTP 1.1)
   end
   
   get '/' do
